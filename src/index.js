c2aecccaf7f56cefe6b1ad94991da95a3af84b21
Oleg Bendiucov <punisher8585@gmail.com>
2023-10-01 11:28:28 +0300

    feat: resolve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split('');
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+
+  bracketsConfig.forEach(element => {
+    const [openin, closing] = element;
+    openingBrackets.push(openin);
+    closingBrackets.push(closing);
+  });
+
+  for (let index = 0; index < arr.length; index += 1) {
+    const element = arr[index];
+
+    if (openingBrackets.includes(element) && closingBrackets.includes(element) && !stack.includes(element)) {
+      stack.push(element);
+    } else if (openingBrackets.includes(element) && !closingBrackets.includes(element)) {
+      stack.push(element);
+    } else if (stack.length) {
+      const openIndex = openingBrackets.indexOf(stack.pop());
+      if (closingBrackets[openIndex] !== element) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return !stack.length;
 }
